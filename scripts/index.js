81a1b977def0bddaae449f1ec85acfd921f657c9
Radik Tarzemanov <schecter.28@yandex.ru>
2021-12-19 19:25:59 +0400

    mesto__commit

--- scripts/index.js
+++ scripts/index.js
@@ -32,27 +32,27 @@ popupButtonCloseForm.addEventListener('click', closePopup);
 popup.addEventListener('click', closePopupByClickOverlay);
 
 // Находим форму в DOM
-let popupSelector = document.querySelector('.popup');
-let popupContainer = popup.querySelector('.popup__container');
-let popupForm = popupContainer.querySelector('.popup__form');
+const popupSelector = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container');
+const popupForm = popupContainer.querySelector('.popup__form');
 
 // Находим поля формы в DOM
-let nameInput = popupForm.querySelector('.popup__name');
-let jobInput = popupForm.querySelector('.popup__job');
+const nameInput = popupForm.querySelector('.popup__name');
+const jobInput = popupForm.querySelector('.popup__job');
 
 //кнопка сохранить
-let saveForm = popupContainer.querySelector('.popup__form-save');
+const saveForm = popupContainer.querySelector('.popup__form-save');
 
 //находим форму поля странице
-let profileNameForm = document.querySelector('.profile__name');	
-let profileJobForm = document.querySelector('profile__job');
+const profileNameForm = document.querySelector('.profile__name');	
+const profileJobForm = document.querySelector('profile__job');
 
 //задаем формулу 
 function changeInform(evt) {
 	evt.preventDefault();
 
-  let profileNameForm = document.querySelector('.profile__name');		
-	let profileJobForm = document.querySelector('.profile__job');
+  const profileNameForm = document.querySelector('.profile__name');		
+	const profileJobForm = document.querySelector('.profile__job');
 
   profileNameForm.textContent = nameInput.value;
   profileJobForm.textContent = jobInput.value;
